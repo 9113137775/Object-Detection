7e0bf152612f3e2938f3201db31b2c4cc368e801
Pranav <105210844+9113137775@users.noreply.github.com>
2023-12-25 19:02:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function ac() {
+    window.location = "ac.html";
+}
+function bedroom() {
+    window.location = "bedroom.html";
+}
+function bulb() {
+    window.location = "bulb.html";
+}
+function fruits() {
+    window.location = "fruits.html";
+}
+function kitchen() {
+    window.location = "kitchen.html";
+}
+function tv() {
+    window.location = "tv.html";
+}
